e5c88d9f4df7d0077a352f4044d4ab1921d7c9a0
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2022-10-11 00:39:56 +0800

    添加蘑菇睡醒call的调用

--- pvz.py
+++ pvz.py
@@ -7,7 +7,7 @@ import win32api
 import ctypes
 import threading
 
-from data import Data, Hack
+from address import Data, Hack
 from run_asm import RunAsm, Reg, wt
 
 
@@ -46,11 +46,30 @@ class PvzModifier:
         return False
 
     def has_user(self):
-        userdata = self.read_offset((self.data.lawn, self.data.user_data))
+        lawn, user_data = self.data.recursively_get_attrs(['lawn', 'user_data'])
+        userdata = self.read_offset((lawn, user_data))
         return userdata != 0
 
     def game_ui(self):
-        return self.read_offset((self.data.lawn, self.data.game_ui))
+        lawn, game_ui = self.data.recursively_get_attrs(['lawn', 'game_ui'])
+        return self.read_offset((lawn, game_ui))
+
+    def scene(self):
+        ui = self.game_ui()
+        if ui == 2 or ui == 3:
+            lawn, board, scene = self.data.recursively_get_attrs(['lawn', 'board', 'scene'])
+            scene = self.read_offset((lawn, board, scene), 4)
+        else:
+            scene = -1
+        return scene
+
+    def get_row_count(self):
+        scene = self.scene()
+        if scene == 2 or scene == 3:
+            return 6
+        elif scene == 0 or scene == 1 or scene == 4 or scene == 5:
+            return 5
+        return -1
 
     def hack(self, hacks: List[Hack], status):
         for hack in hacks:
@@ -98,59 +117,60 @@ class PvzModifier:
 
     def sun_shine(self, number):
         if isinstance(number, int):
-            sun_addr = self.read_offset((self.data.lawn, self.data.board)) + self.data.sun
-            self.write_memory(sun_addr, number, 4)
+            lawn, board, sun = self.data.recursively_get_attrs(['lawn', 'board', 'sun'])
+            self.write_offset((lawn, board, sun), number, 4)
 
     def money(self, number):
         if not self.has_user():
             return
         if isinstance(number, int):
-            money_addr = self.read_offset((self.data.lawn, self.data.user_data)) + self.data.money
-            self.write_memory(money_addr, number // 10, 4)
+            lawn, user_data, money = self.data.recursively_get_attrs(['lawn', 'user_data', 'money'])
+            self.write_offset((lawn, user_data, money), number // 10, 4)
 
     def adventure(self, number):
         if not self.has_user():
             return
         if isinstance(number, int):
-            level_addr = self.read_offset((self.data.lawn, self.data.user_data)) + self.data.level
-            adventure_level = self.read_offset((self.data.lawn, self.data.board)) + self.data.adventure_level
-            self.write_memory(level_addr, number, 4)
-            self.write_memory(adventure_level, number, 4)
+            lawn = self.data.lawn
+            user_data, level = lawn.recursively_get_attrs(['user_data', 'level'])
+            board, adventure_level = lawn.recursively_get_attrs(['board', 'adventure_level'])
+            self.write_offset((lawn, user_data, level), number, 4)
+            self.write_offset((lawn, board, adventure_level), number, 4)
 
     def tree_height(self, number):
         if not self.has_user():
             return
         if isinstance(number, int):
-            tree_height_addr = self.read_offset((self.data.lawn, self.data.user_data)) + self.data.tree_height
-            self.write_memory(tree_height_addr, number, 4)
+            lawn, user_data, tree_height = self.data.recursively_get_attrs(['lawn', 'user_data', 'tree_height'])
+            self.write_offset((lawn, user_data, tree_height), number, 4)
 
     def fertilizer(self, number):
         if not self.has_user():
             return
         if isinstance(number, int):
-            fer_addr = self.read_offset((self.data.lawn, self.data.user_data)) + self.data.fertilizer
-            self.write_memory(fer_addr, number + 1000, 4)
+            lawn, user_data, fertilizer = self.data.recursively_get_attrs(['lawn', 'user_data', 'fertilizer'])
+            self.write_offset((lawn, user_data, fertilizer), number + 1000, 4)
 
     def bug_spray(self, number):
         if not self.has_user():
             return
         if isinstance(number, int):
-            bug_spray_addr = self.read_offset((self.data.lawn, self.data.user_data)) + self.data.bug_spray
-            self.write_memory(bug_spray_addr, number + 1000, 4)
+            lawn, user_data, bug_spray = self.data.recursively_get_attrs(['lawn', 'user_data', 'bug_spray'])
+            self.write_offset((lawn, user_data, bug_spray), number + 1000, 4)
 
     def chocolate(self, number):
         if not self.has_user():
             return
         if isinstance(number, int):
-            chocolate_addr = self.read_offset((self.data.lawn, self.data.user_data)) + self.data.chocolate
-            self.write_memory(chocolate_addr, number + 1000, 4)
+            lawn, user_data, chocolate = self.data.recursively_get_attrs(['lawn', 'user_data', 'chocolate'])
+            self.write_offset((lawn, user_data. chocolate), number + 1000, 4)
 
     def tree_food(self, number):
         if not self.has_user():
             return
         if isinstance(number, int):
-            tree_food_addr = self.read_offset((self.data.lawn, self.data.user_data)) + self.data.tree_food
-            self.write_memory(tree_food_addr, number + 1000, 4)
+            lawn, user_data, tree_food = self.data.recursively_get_attrs(['lawn', 'user_data', 'tree_food'])
+            self.write_offset((lawn, user_data, tree_food), number + 1000, 4)
 
     def vase_transparent(self, status=True):
         self.hack(self.data.vase_transparent, status)
@@ -180,11 +200,11 @@ class PvzModifier:
         self.hack(self.data.tree_food_not_dec, status)
 
     def lock_shovel(self, status=True):
-        cursor_grab_addr = self.read_offset((self.data.lawn, self.data.board, self.data.cursor)) + self.data.cursor_grab
+        lawn, board, cursor, cursor_grab = self.data.recursively_get_attrs(['lawn', 'board', 'cursor', 'cursor_grab'])
         if status:
-            self.write_memory(cursor_grab_addr, 6, 4)
+            self.write_offset((lawn, board, cursor, cursor_grab), 6, 4)
         else:
-            self.write_memory(cursor_grab_addr, 0, 4)
+            self.write_offset((lawn, board, cursor, cursor_grab), 0, 4)
         self.hack(self.data.lock_shovel, status)
 
     def unlock_limbo_page(self, status=True):
@@ -194,27 +214,29 @@ class PvzModifier:
         self.hack(self.data.background_running, status)
 
     def set_speed_rate(self, rate):
-        frame_duration = int(10 // rate)
-        self.write_offset((self.data.lawn, self.data.frame_duration), frame_duration, 4)
+        new_duration = int(10 // rate)
+        lawn, frame_duration = self.data.recursively_get_attrs(['lawn', 'frame_duration'])
+        self.write_offset((lawn, frame_duration), new_duration, 4)
 
     def unlock_game(self):
         if not self.has_user():
             return
-        user_data = self.read_offset((self.data.lawn, self.data.user_data))
-        playthrough_addr = user_data + self.data.playthrough
+        lawn, user_data, playthrough = self.data.recursively_get_attrs(['lawn', 'user_data', 'playthrough'])
+        user_data_addr = self.read_offset((lawn, user_data))
+        playthrough_addr = user_data_addr + playthrough
         playthrough = self.read_memory(playthrough_addr, 4)
         if playthrough < 2:
             self.write_memory(playthrough_addr, 2, 4)
-        survival_addr = user_data + self.data.survival
+        survival_addr = user_data_addr + user_data.survival
         self.loop_write_memory(survival_addr, [5, 5, 5, 5, 5, 10, 10, 10, 10, 10], 4)
-        mini_game_addr = user_data + self.data.mini_game
-        puzzle_addr = user_data + self.data.puzzle
+        mini_game_addr = user_data_addr + user_data.mini_game
+        puzzle_addr = user_data_addr + user_data.puzzle
         mini_game_flags = self.loop_read_memory(mini_game_addr, 20, 4)
         puzzle_flags = self.loop_read_memory(puzzle_addr, 20, 4)
         self.loop_write_memory(mini_game_addr, map(lambda x: x or 1, mini_game_flags), 4)
         self.loop_write_memory(puzzle_addr, map(lambda x: x or 1, puzzle_flags), 4)
         """8种紫卡与模仿者"""
-        twiddydinky_addr = user_data + self.data.twiddydinky
+        twiddydinky_addr = user_data_addr + user_data.twiddydinky
         self.loop_write_memory(twiddydinky_addr, [1] * 9, 4)
         twiddydinky_addr += 9 * 4
         """园艺手套、蘑菇园等"""
@@ -240,19 +262,24 @@ class PvzModifier:
         self.chocolate(999)
 
         if playthrough == 0 and self.game_ui() == 1:
+            lawn, game_selector = self.data.recursively_get_attrs(['lawn', 'game_selector'])
+            call_main_menu = lawn.call_main_menu
             self.asm.asm_init()
             self.asm.asm_push_byte(1)
-            self.asm.asm_mov_exx_dword_ptr(Reg.ECX, self.data.lawn)
-            self.asm.asm_mov_exx_dword_ptr_exx_add(Reg.ECX, self.data.game_selector)
-            self.asm.asm_call(self.data.call_main_menu)
+            self.asm.asm_mov_exx_dword_ptr(Reg.ECX, lawn)
+            self.asm.asm_mov_exx_dword_ptr_exx_add(Reg.ECX, game_selector)
+            self.asm.asm_mov_exx_exx(Reg.ESI, Reg.ECX)
+            self.asm.asm_call(call_main_menu)
             self.asm.asm_ret()
+            self.lock.acquire()
             self.asm.asm_code_inject(self.phand)
+            self.lock.release()
 
     def unlock_achievements(self):
         if not self.has_user():
             return
-        user_data = self.read_offset((self.data.lawn, self.data.user_data))
-        achievement_address = user_data + self.data.achievement
+        lawn, user_data, achievement = self.data.recursively_get_attrs(['lawn', 'user_data', 'achievement'])
+        achievement_address = self.read_offset((lawn, user_data)) + achievement
         self.loop_write_memory(achievement_address, [1] * 20, 1)
 
     def plant_invincible(self, status=True):
@@ -284,6 +311,8 @@ class PvzModifier:
 
     def mushroom_awake(self, status=True):
         self.hack(self.data.mushrooms_awake, status)
+        if status:
+            self.set_mushroom_awake()
 
     def zombie_invincible(self, status=True):
         self.hack(self.data.zombie_immune_body_damage, status)
@@ -303,6 +332,76 @@ class PvzModifier:
         self.hack(self.data.zombie_helm_weak, status)
         self.hack(self.data.zombie_shield_weak, status)
         self.hack(self.data.zombie_can_burn_crumble, status)
+
+    def asm_put_plant(self, plant_type, row, col, imitator):
+        lawn, board = self.data.recursively_get_attrs(['lawn', 'board'])
+        if imitator:
+            self.asm.asm_push_dword(plant_type)
+            self.asm.asm_push_dword(0x30)
+        else:
+            self.asm.asm_push_dword(0xffffffff)
+            self.asm.asm_push_dword(plant_type)
+        self.asm.asm_mov_exx(Reg.EAX, row)
+        self.asm.asm_push_dword(col)
+        self.asm.asm_mov_exx_dword_ptr(Reg.EDI, lawn)
+        self.asm.asm_mov_exx_dword_ptr_exx_add(Reg.EDI, board)
+        self.asm.asm_push_exx(Reg.EDI)
+        self.asm.asm_call(0x418d70)
+
+    def put_plant(self, plant_type, row, col, imitator):
+        ui = self.game_ui()
+        if ui != 2 and ui != 3:
+            return
+        row_count = self.get_row_count()
+        col_count = 8 if plant_type == 47 else 9
+        width = 2 if plant_type == 47 else 1
+        if row >= row_count or col >= col_count:
+            return
+        self.asm.asm_init()
+        if row == -1 and col == -1:
+            for r in range(row_count):
+                for c in range(0, col_count, width):
+                    self.asm_put_plant(plant_type, r, c, imitator)
+        elif row == -1 and col != -1:
+            for r in range(row_count):
+                self.asm_put_plant(plant_type, r, col, imitator)
+        elif row != -1 and col == -1:
+            for c in range(0, col_count, width):
+                self.asm_put_plant(plant_type, row, c, imitator)
+        else:
+            self.asm_put_plant(plant_type, row, col, imitator)
+        self.asm.asm_ret()
+        self.lock.acquire()
+        self.asm.asm_code_inject(self.phand)
+        self.lock.release()
+
+    def set_mushroom_awake(self):
+        ui = self.game_ui()
+        if ui != 2 and ui != 3:
+            return
+        lawn, board, plant_count_max = self.data.recursively_get_attrs(['lawn', 'board', 'plant_count_max'])
+        plants = board.plants
+        plants_addr = self.read_offset((lawn, board, plants))
+        plant_size = 0x14c
+        plant_dead = plants.dead
+        plant_squished = plants.squished
+        plant_asleep = plants.asleep
+        plant_count_max = self.read_offset((lawn, board, plant_count_max), 4)
+        self.asm.asm_init()
+        for i in range(plant_count_max):
+            addr = plants_addr + plant_size * i
+            is_dead = self.read_memory(addr + plant_dead, 1)
+            is_squished = self.read_memory(addr + plant_squished, 1)
+            is_asleep = self.read_memory(addr + plant_asleep, 1)
+            if is_dead or is_squished or not is_asleep:
+                continue
+            self.asm.asm_mov_exx(Reg.EDI, addr)
+            self.asm.asm_push_byte(0)
+            self.asm.asm_call(self.data.call_set_plant_sleeping)
+        self.asm.asm_ret()
+        self.lock.acquire()
+        self.asm.asm_code_inject(self.phand)
+        self.lock.release()
 
 
 if __name__ == '__main__':
